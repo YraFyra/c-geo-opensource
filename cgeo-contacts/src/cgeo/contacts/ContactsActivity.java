5a4e6b00f1df963dc5b06506114ec3667087d1ae
YraFyra <yrafyra@gmail.com>
2014-02-20 02:38:31 +0100

    Better lookup and selection of Contacts
    Disclaimer: I know this code is looking really bad in many ways. My bad.
    I'm not writing that much Java UI code...
    Feel free to copy things that are useful.

--- cgeo-contacts/src/cgeo/contacts/ContactsActivity.java
+++ cgeo-contacts/src/cgeo/contacts/ContactsActivity.java
@@ -4,21 +4,28 @@ import org.apache.commons.lang3.CharEncoding;
 import org.apache.commons.lang3.StringUtils;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.BaseColumns;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.util.Pair;
 import android.view.Gravity;
 import android.widget.Toast;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
 
-public final class ContactsActivity extends Activity {
+public final class ContactsActivity extends Activity implements DialogInterface.OnClickListener
+{
     static final String LOG_TAG = "cgeo.contacts";
+
+    private List<Pair<Integer, String>> contacts = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,40 +44,64 @@ public final class ContactsActivity extends Activity {
         }
 
         // search by nickname
-        int contactId = getContactId(nickName, ContactsContract.Data.CONTENT_URI, ContactsContract.Data.CONTACT_ID, ContactsContract.CommonDataKinds.Nickname.NAME);
+        contacts = getContacts(nickName, ContactsContract.Data.CONTENT_URI, ContactsContract.Data.CONTACT_ID, ContactsContract.CommonDataKinds.Nickname.NAME);
 
         // search by display name
-        if (contactId == 0) {
-            contactId = getContactId(nickName, ContactsContract.Contacts.CONTENT_URI, BaseColumns._ID, ContactsContract.Contacts.DISPLAY_NAME);
+        if (contacts.size() == 0) {
+            contacts = getContacts(null, Uri.withAppendedPath(ContactsContract.AUTHORITY_URI, "data/phones/filter/" + nickName), ContactsContract.Data.CONTACT_ID, ContactsContract.Contacts.DISPLAY_NAME);
         }
 
-        if (contactId == 0) {
+        if (contacts.size() == 0) {
             showToast(getString(R.string.contact_not_found, nickName));
             finish();
             return;
+        } else if (contacts.size() == 1) {
+            openContact(contacts.get(0));
+        } else {
+            List<String> list = new ArrayList<String>();
+            for (Pair<Integer, String> p : contacts) {
+                list.add(p.second);
+            }
+            final CharSequence[] items = list.toArray(new CharSequence[list.size()]);
+            new AlertDialog.Builder(this)
+                    .setTitle("Select user")
+                    .setItems(items, this)
+                    .create().show();
+            return;
         }
-
-        openContact(contactId);
         finish();
     }
 
-    private void openContact(int id) {
+    public void onClick(DialogInterface dialog, int which) {
+        dialog.dismiss();
+        openContact(contacts.get(which));
+    }
+
+    private void openContact(Pair<Integer, String> pair) {
         final Intent intent = new Intent(Intent.ACTION_VIEW);
-        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, String.valueOf(id));
+        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, String.valueOf(pair.first));
         intent.setData(uri);
         startActivity(intent);
     }
 
-    private int getContactId(final String searchName, Uri uri, final String idColumnName, final String selectionColumnName) {
-        int foundId = 0;
-        final String[] projection = new String[] { idColumnName };
-        final String selection = selectionColumnName + " = ? COLLATE NOCASE";
-        final String[] selectionArgs = new String[] { searchName };
+    private List<Pair<Integer, String>> getContacts(final String searchName, Uri uri, final String idColumnName, final String selectionColumnName) {
+        Integer foundId = 0;
+        List<Pair<Integer, String>> list = new ArrayList<Pair<Integer, String>>();
+        final String[] projection = new String[] { idColumnName, selectionColumnName };
+        String selection = null;
+        String[] selectionArgs = null;
+        if (searchName != null) {
+            selection = selectionColumnName + " LIKE ? COLLATE NOCASE";
+            selectionArgs = new String[] { searchName };
+        }
         Cursor cursor = null;
         try {
             cursor = getContentResolver().query(uri, projection, selection, selectionArgs, null);
-            if (cursor != null && cursor.moveToNext()) {
-                foundId = cursor.getInt(0);
+            while (cursor != null && !cursor.isLast() && cursor.moveToNext()) {
+                foundId = Integer.valueOf(cursor.getInt(0));
+                String foundName = cursor.getString(1);
+                Log.d("geodaniel", "Found user " + foundName + " " + foundId);
+                list.add(new Pair(foundId, foundName));
             }
         } catch (Exception e) {
             Log.e(LOG_TAG, "ContactsActivity.getContactId", e);
@@ -79,7 +110,7 @@ public final class ContactsActivity extends Activity {
                 cursor.close();
             }
         }
-        return foundId;
+        return list;
     }
 
     public final void showToast(final String text) {
